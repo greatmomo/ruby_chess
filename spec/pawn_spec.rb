db5cdccfa05558672b25e2a218105ac1ce9eda1a
greatmomo <66915435+greatmomo@users.noreply.github.com>
2022-03-03 18:31:24 +0100

    add pawn and piece framework

---
+++ spec/pawn_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative '../lib/pawn'
+
+describe Piece do
+  describe '#initialize' do
+    # check if piece is created properly?
+  end
+
+  # make sure it can access Piece methods
+end
---
+++ spec/piece_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative '../lib/piece'
+
+describe Piece do
+  describe '#initialize' do
+    # check if piece is created properly?
+  end
+end
