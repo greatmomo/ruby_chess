750669b72b2936ab9d62a504e0914c9dd8a86875
greatmomo <66915435+greatmomo@users.noreply.github.com>
2022-03-06 15:31:45 +0100

    board init

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,37 @@
 # frozen_string_literal: true
 
-class Board
+require_relative 'pawn'
 
+class Board
+  attr_reader :squares, :white_to_move
+
+  def initialize
+    @squares = []
+    @white_to_move = true
+    8.times { |i| @squares[i] = [] }
+    fill_board
+    # add_position
+    # set_moves_and_captures
+  end
+
+  def fill_board
+    # squares[0][0] = WhiteRook.new(self, [0, 0])
+    # squares[7][0] = WhiteRook.new(self, [7, 0])
+    # squares[1][0] = WhiteKnight.new(self, [1, 0])
+    # squares[6][0] = WhiteKnight.new(self, [6, 0])
+    # squares[2][0] = WhiteBishop.new(self, [2, 0])
+    # squares[5][0] = WhiteBishop.new(self, [5, 0])
+    # squares[3][0] = WhiteQueen.new(self, [3, 0])
+    # squares[4][0] = WhiteKing.new(self, [4, 0])
+    8.times { |i| squares[i][1] = WhitePawn.new(self, [i, 1]) }
+    # squares[0][7] = BlackRook.new(self, [0, 7])
+    # squares[7][7] = BlackRook.new(self, [7, 7])
+    # squares[1][7] = BlackKnight.new(self, [1, 7])
+    # squares[6][7] = BlackKnight.new(self, [6, 7])
+    # squares[2][7] = BlackBishop.new(self, [2, 7])
+    # squares[5][7] = BlackBishop.new(self, [5, 7])
+    # squares[3][7] = BlackQueen.new(self, [3, 7])
+    # squares[4][7] = BlackKing.new(self, [4, 7])
+    8.times { |i| squares[i][6] = BlackPawn.new(self, [i, 6]) }
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,6 +4,21 @@ require_relative '../lib/board'
 
 describe Board do
   describe '#initialize' do
-    # ???
+    # needs to build a board with the required squares and all the expected pieces
+    context 'has a white pawn in the correct position' do
+      subject(:board_init) { described_class.new }
+
+      it 'should have a white pawn at [1][1]' do
+        expect(board_init.squares[1][1]).to be_kind_of(WhitePawn)
+      end
+    end
+
+    context 'has a black pawn in the correct position' do
+      subject(:board_init) { described_class.new }
+
+      it 'should have a black pawn at [1][6]' do
+        expect(board_init.squares[6][6]).to be_kind_of(BlackPawn)
+      end
+    end
   end
 end
