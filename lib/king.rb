5e14c6b7debedcd2aa874d923ea07c69d300b95c
greatmomo <66915435+greatmomo@users.noreply.github.com>
2022-03-09 17:59:09 +0100

    copy spec and implementation from pawn
    skip moved since it's not relevant
    adjust king_spec to match reality

--- lib/king.rb
+++ lib/king.rb
@@ -12,6 +12,31 @@ class King < Piece
   def to_s
     white? ? '♚'.gray : '♚'.black
   end
+
+  def set_valid_moves
+    moves = []
+    file = location[0]
+    rank = location[1] + 1 * dir
+    unless has_moved?
+      moves << [file, rank + 1 * dir] if board.squares[file][rank + 1 * dir].nil? &&
+                                         board.squares[file][rank].nil?
+    end
+    moves << [file, rank] if rank.between?(0, Board::MAX) && board.squares[file][rank].nil?
+    @valid_moves = moves
+  end
+
+  def set_valid_captures
+    moves = []
+    file = location[0]
+    rank = location[1] + 1 * dir
+    unless board.squares[file + 1][rank].nil? 
+      moves << [file + 1, rank] if board.squares[file - 1][rank].white? != white?
+    end
+    unless board.squares[file - 1][rank].nil? 
+      moves << [file - 1, rank] if board.squares[file - 1][rank].white? != white?
+    end
+    @valid_captures = moves
+  end
 end
 
 class WhiteKing < King
