2414db4c3395697a8859c0c5b7f26243bcdb3b89
greatmomo <66915435+greatmomo@users.noreply.github.com>
2022-03-11 19:41:51 +0100

    straights open tiles works

--- spec/direction_spec.rb
+++ spec/direction_spec.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
 require_relative '../lib/direction'
+require_relative '../lib/rook'
+require_relative '../lib/board'
 
 describe Direction do
   describe '#direction_scan' do
     # check if valid moves array exists?
     context 'straights' do
-      
-      it 'returns two arrays' do
-        expect(Direction.direction_scan).to eq([[[0,0],[0,1]],[[0,2]]])
+      it 'returns two proper arrays' do
+        expect(Direction.direction_scan(Direction.straights, Board.new, [5, 4], "white")).to eq(
+          [[[5, 5],[5, 3],[5, 2],[0, 4],[1, 4],[2, 4],[3, 4],[4, 4],[6, 4],[7, 4]],
+          [[5, 6]]])
       end
     end
   end
