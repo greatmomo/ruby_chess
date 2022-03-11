4c601695a264ba2b321a80164a38915e6ad1bf36
greatmomo <66915435+greatmomo@users.noreply.github.com>
2022-03-11 19:58:28 +0100

    omni works too

--- spec/direction_spec.rb
+++ spec/direction_spec.rb
@@ -26,10 +26,11 @@ describe Direction do
 
     context 'both' do
       it 'returns two proper arrays' do
-        expect(Direction.direction_scan(Direction.omni, Board.new, [2, 3], "white")[0]).to include(
-          [1, 4],[0, 5],[3, 4],[4, 5],[3, 2],[1, 2])
-        expect(Direction.direction_scan(Direction.omni, Board.new, [2, 3], "white")[1]).to include(
-          [0, 1],[4, 1])
+        expect(Direction.direction_scan(Direction.omni, Board.new, [4, 4], "white")[0]).to include(
+          [3, 5],[4, 5],[5, 5],[0, 4],[1, 4],[2, 4],[3, 4],[5, 4],[6, 4],[7, 4],[3, 3],
+          [4, 3],[5, 3],[2, 2],[4, 2],[6, 2])
+        expect(Direction.direction_scan(Direction.omni, Board.new, [4, 4], "white")[1]).to include(
+          [2, 6],[4, 6],[6, 6])
       end
     end
   end
