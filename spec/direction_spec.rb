e50aed3bba0c9f030eb7ea6c45733633a997d9ff
greatmomo <66915435+greatmomo@users.noreply.github.com>
2022-03-11 19:54:05 +0100

    diagonals

--- spec/direction_spec.rb
+++ spec/direction_spec.rb
@@ -6,13 +6,30 @@ require_relative '../lib/board'
 
 describe Direction do
   describe '#direction_scan' do
-    # check if valid moves array exists?
     context 'straights' do
       it 'returns two proper arrays' do
         expect(Direction.direction_scan(Direction.straights, Board.new, [5, 4], "white")[0]).to include(
           [5, 5],[5, 3],[5, 2],[0, 4],[1, 4],[2, 4],[3, 4],[4, 4],[6, 4],[7, 4])
         expect(Direction.direction_scan(Direction.straights, Board.new, [5, 4], "white")[1]).to include(
           [5, 6])
+      end
+    end
+
+    context 'diagonals' do
+      it 'returns two proper arrays' do
+        expect(Direction.direction_scan(Direction.diagonals, Board.new, [2, 3], "black")[0]).to include(
+          [1, 4],[0, 5],[3, 4],[4, 5],[3, 2],[1, 2])
+        expect(Direction.direction_scan(Direction.diagonals, Board.new, [2, 3], "black")[1]).to include(
+          [0, 1],[4, 1])
+      end
+    end
+
+    context 'both' do
+      it 'returns two proper arrays' do
+        expect(Direction.direction_scan(Direction.omni, Board.new, [2, 3], "white")[0]).to include(
+          [1, 4],[0, 5],[3, 4],[4, 5],[3, 2],[1, 2])
+        expect(Direction.direction_scan(Direction.omni, Board.new, [2, 3], "white")[1]).to include(
+          [0, 1],[4, 1])
       end
     end
   end
